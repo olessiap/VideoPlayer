964623b84f0a5546b518f169c879fb52e626231b
Olessia Potapova <olessia.potapova@gmail.com>
2019-04-22 12:43:49 -0300

    added functionality to progress bar

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
+///ELEMENTS///
 let playButton = document.querySelector(".toggle");
-let videoFile = document.querySelector(".viewer");
+let vid = document.querySelector(".viewer");
+let progressBar = document.querySelector(".progress__filled");
+let progressStyle = window.getComputedStyle(progressBar);
 
+
+///EVENT LISTENERS///
 playButton.addEventListener("click", playFunction);
+vid.addEventListener('timeupdate', updateProgressBar, false);
 
+//PLAY/PAUSE THE VIDEO///
 window.keydown(function(e) {
     if(e.key === " " || e.key ==="Spacebar") {
         e.preventDefault();
         playFunction();
     }
-});
-
+}); 
 
 function playFunction() {
-    if(videoFile.paused) {
-        videoFile.play();
+    if(vid.paused) {
+        vid.play();
         playButton.innerHTML="=";
+        updateProgressBar();
     } else {
-        videoFile.pause();
+        vid.pause();
         playButton.innerHTML="►";
+        pauseProgressBar();
     }
 }
+
+///UPDATE PROGRESS BAR///
+function updateProgressBar() {
+    progressBar.style.flexBasis = "0%";
+    let percentage = Math.floor((100 / vid.duration) * vid.currentTime);
+    console.log(percentage);
+    progressBar.style.flexBasis = percentage + "%";
+}
+
+function pauseProgressBar() {
+    console.log("paused");
+}
+
+
+//play/pause video by pressing play/pause button and space bar
+//track the video playing and update progress bar
+//click anywhere on progress bar to start video from there
+//skip forward/rewind back by 10 seconds  --> use currentTime = 10;
+//reset the player to 0 progress on refresh 
